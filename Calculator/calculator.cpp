10ea2f85c852478feb332756cf7d5f242336bf3f
denverinthrough-crypto <denverinthrough@gmail.com>
2026-02-25 13:30:24 -0500

    modify and update calculator

--- Calculator/calculator.cpp
+++ Calculator/calculator.cpp
@@ -2,58 +2,57 @@
 using namespace std;
 
 class Calculator {
-    private: // Accesss identifiers
-        double num1; // attributes
-        double num2; // attributes
+private:
+    double num1;
+    double num2;
 
-    public: // Access identifiers
-        
-        // Function to get input from user
-        void getInput() {
-            cout << "Enter first number: ";
-            cin >> num1;
+public:
+    void getInput() {
+        cout << "Enter first number: ";
+        cin >> num1;
 
-            cout << "Enter second number: ";
-            cin >> num2;
+        cout << "Enter second number: ";
+        cin >> num2;
+    }
+
+    double add() {
+        return num1 + num2;
+    }
+
+    double subtract() {
+        return num1 - num2;
+    }
+
+    double multiply() {
+        return num1 * num2;
+    }
+
+    double division() {
+        if (num2 == 0) {
+            cout << "Error: Cannot divide by zero.\n";
+            return 0;
         }
-
-        double add() {      // double for floating or decimal points
-            return num1 + num2;
-        }
-
-        double subtract() {
-            return num1 - num2;
-        }
-
-        double multiply() {
-            return num1 * num2;
-        }
-
-        double division() {
-            return num2 / num2;
-        }
+        return num1 / num2;
+    }
 };
 
-// Main Program
-
 int main() {
-    Calculator calc; // create object of Calculator
-    char choice; // single character like numbers and letters to choose.
-    char again; // 
+    Calculator calc;
+    char choice;
+    char again;
 
     do {
-        cout << "\n ====== Simple OOP Calculator =====\n";
-        cout  << "\n";
+        cout << "\n===== Simple OOP Calculator =====\n";
         cout << "Choose operation (+, -, *, /): ";
         cin >> choice;
 
         calc.getInput();
-        
+
         switch(choice) {
             case '+':
                 cout << "Result: " << calc.add() << endl;
                 break;
-            
+
             case '-':
                 cout << "Result: " << calc.subtract() << endl;
                 break;
@@ -64,7 +63,8 @@ int main() {
 
             case '/':
                 cout << "Result: " << calc.division() << endl;
-
+                break; // ✅ important!
+            
             default:
                 cout << "Invalid operator.\n";
         }
@@ -75,7 +75,5 @@ int main() {
     } while (again == 'y' || again == 'Y');
 
     cout << "Goodbye!\n";
-
     return 0;
-    
 }
