1d4d3d6d4cc5f6a0b264b9594d632abb493844a8
denverinthrough-crypto <denverinthrough@gmail.com>
2026-02-27 12:32:50 -0500

    Implementing CRUD

--- NameList/nameList.cpp
+++ NameList/nameList.cpp
@@ -18,27 +18,111 @@ class Namelist {
         }
 
         // Display all names
-        void showNames() {
+        void showNames() const {
             if (names.empty()) {
                 cout << "No names in the list.\n";
+                return;
             }
-            else {
-                cout << "Names in the list:\n";
-                for (const string& name :  names) {
-                    cout << "- " << name << endl;
-                }
+            for (size_t i = 0; i < names.size(); i++) {
+                cout << i << ": " << names[i] << endl;
+            
             }
         }
+
+        // Search name
+
+        void searchName(const string& name) const {
+            for (size_t i = 0; i < names.size(); i++) {
+                if (names[i] == name) {
+                    cout << name << " found at index " << i << endl;
+                    return;
+                }
+            }
+            cout << name << " not found.\n";
+        }
+
+        // Remove Name
+        void removeName(size_t index) {
+            if(index >= names.size()) {
+                cout << "Invalid index.\n";
+                return;
+            }
+            cout << "Removed: " << names[index] << endl;
+            names.erase(names.begin() + index);
+        }
+
+        // Modify names
+        void modifyName(size_t index, const string& newName) {
+            if(index >= names.size()) {
+                cout << "Invalid index.\n";
+                return;
+            }
+            names[index] = newName;
+            cout << "Updated successfully.\n";
+        }
+
+        // Access name
+        void accessName(size_t index) const {
+            if(index >= names.size()) {
+                cout << "Invalid index.\n";
+                return;
+            }
+            cout << "Name: " << names[index] << endl;
+        }
+
 };
 
 int main() {
     Namelist myList;
+    int choice;
 
-    myList.addName("Denver");
-    myList.addName("Alice");
-    myList.addName("Bob");
+   do {
+    cout << "\n1. Add\n2. Show\n3. Search\n4. Remove\n5. Modify\n6. Access\n0. Exit\nChoice: ";
+    cin >> choice;
 
-    myList.showNames();
+    if (choice == 1) {
+        string name;
+        cout << "Enter name: ";
+        cin >> name;
+        myList.addName(name);
+
+    } else if (choice == 2) {
+        myList.showNames();
+    
+    } else if (choice == 3) {
+        string name;
+        cout << "Search name: ";
+        cin >> name;
+        myList.searchName(name);
+
+    } else if (choice == 4) {
+        size_t index;
+        cout << "Index to remove: ";
+        cin >> index;
+        myList.removeName(index);
+
+    } else if (choice == 5) {
+        size_t index;
+        string name;
+
+        cout << "Index to modify: ";
+        cin >> index;
+        cout << "New Name: ";
+        cin >> name;
+
+        myList.modifyName(index, name);
+
+    } else if (choice == 6) {
+        size_t index;
+        cout << "Index to access: ";
+        cin >> index;
+        myList.accessName(index);
+    }
+
+   } while (choice != 0);
+
+   cout << "Goodbye\n";
+
 
     return 0;
 }
