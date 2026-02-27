d185f90975c36d1af81b73916e94a45e61c79382
denverinthrough-crypto <denverinthrough@gmail.com>
2026-02-27 13:42:07 -0500

    converting names from lower to uppercase

--- NameList/nameList.cpp
+++ NameList/nameList.cpp
@@ -1,23 +1,29 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cctype>
+#include <algorithm>
+#include <limits>
 using namespace std;
-
 
 class Namelist {
     private:
-        vector<string> names; // store names
+        vector<string> names;
 
-    public:
-        // Add a name to the list
-
-        void addName(const string& name) {
-            names.push_back(name);
-            cout << "Added: " << name << endl;
-
+        string toUpper(string text) const {
+            transform(text.begin(), text.end(), text.begin(),
+                [](unsigned char c) { return toupper(c); });
+            return text;
         }
 
-        // Display all names
+    public:
+
+        void addName(const string& name) {
+            string upperName = toUpper(name);
+            names.push_back(upperName);
+            cout << "Added: " << upperName << endl;
+        }
+
         void showNames() const {
             if (names.empty()) {
                 cout << "No names in the list.\n";
@@ -25,25 +31,22 @@ class Namelist {
             }
             for (size_t i = 0; i < names.size(); i++) {
                 cout << i << ": " << names[i] << endl;
-            
             }
         }
 
-        // Search name
-
         void searchName(const string& name) const {
+            string upperName = toUpper(name);
             for (size_t i = 0; i < names.size(); i++) {
-                if (names[i] == name) {
-                    cout << name << " found at index " << i << endl;
+                if (names[i] == upperName) {
+                    cout << upperName << " found at index " << i << endl;
                     return;
                 }
             }
-            cout << name << " not found.\n";
+            cout << upperName << " not found.\n";
         }
 
-        // Remove Name
         void removeName(size_t index) {
-            if(index >= names.size()) {
+            if (index >= names.size()) {
                 cout << "Invalid index.\n";
                 return;
             }
@@ -51,79 +54,86 @@ class Namelist {
             names.erase(names.begin() + index);
         }
 
-        // Modify names
         void modifyName(size_t index, const string& newName) {
-            if(index >= names.size()) {
+            if (index >= names.size()) {
                 cout << "Invalid index.\n";
                 return;
             }
-            names[index] = newName;
+            names[index] = toUpper(newName);
             cout << "Updated successfully.\n";
         }
 
-        // Access name
         void accessName(size_t index) const {
-            if(index >= names.size()) {
+            if (index >= names.size()) {
                 cout << "Invalid index.\n";
                 return;
             }
             cout << "Name: " << names[index] << endl;
         }
-
 };
 
 int main() {
     Namelist myList;
-    int choice;
+    int choice = -1;
 
-   do {
-    cout << "\n1. Add\n2. Show\n3. Search\n4. Remove\n5. Modify\n6. Access\n0. Exit\nChoice: ";
-    cin >> choice;
+    do {
+        cout << " ====== NAME-LIST ===== \n";
+        cout << "\n1. Add\n2. Show\n3. Search\n4. Remove\n5. Modify\n6. Access\n0. Exit\nChoice: ";
+        
+        string input;
+        getline(cin, input);  // read everything as string, no cin failure possible
 
-    if (choice == 1) {
-        string name;
-        cout << "Enter name: ";
-        cin >> name;
-        myList.addName(name);
-
-    } else if (choice == 2) {
-        myList.showNames();
+        try {
+            choice = stoi(input);  // try to convert to int
+        } catch (...) {
+            cout << "Invalid input. Please enter a number from 1 to 6: \n";
+            continue;
+        }
+        
     
-    } else if (choice == 3) {
-        string name;
-        cout << "Search name: ";
-        cin >> name;
-        myList.searchName(name);
 
-    } else if (choice == 4) {
-        size_t index;
-        cout << "Index to remove: ";
-        cin >> index;
-        myList.removeName(index);
+        if (choice == 1) {
+            string name;
+            cout << "Enter name: ";
+            getline(cin, name);
+            myList.addName(name);
 
-    } else if (choice == 5) {
-        size_t index;
-        string name;
+        } else if (choice == 2) {
+            myList.showNames();
 
-        cout << "Index to modify: ";
-        cin >> index;
-        cout << "New Name: ";
-        cin >> name;
+        } else if (choice == 3) {
+            string name;
+            cout << "Search name: ";
+            getline(cin, name);
+            myList.searchName(name);
 
-        myList.modifyName(index, name);
+        } else if (choice == 4) {
+            size_t index;
+            cout << "Index to remove, pick from 0 to 4: ";
+            cin >> index;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            myList.removeName(index);
 
-    } else if (choice == 6) {
-        size_t index;
-        cout << "Index to access: ";
-        cin >> index;
-        myList.accessName(index);
-    }
+        } else if (choice == 5) {
+            size_t index;
+            string name;
+            cout << "Index to modify: ";
+            cin >> index;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "New Name: ";
+            getline(cin, name);
+            myList.modifyName(index, name);
 
-   } while (choice != 0);
+        } else if (choice == 6) {
+            size_t index;
+            cout << "Index to access from 0 to 4: ";
+            cin >> index;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            myList.accessName(index);
+        }
 
-   cout << "Goodbye\n";
+    } while (choice != 0);
 
-
+    cout << "Goodbye\n";
     return 0;
 }
-    
