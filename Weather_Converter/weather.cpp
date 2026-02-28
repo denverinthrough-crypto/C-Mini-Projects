93969fc49470085a5b17e852c78694ed3a5810bc
denverinthrough-crypto <denverinthrough@gmail.com>
2026-02-28 14:24:11 -0500

    renaming parameters and created message function

--- Weather_Converter/weather.cpp
+++ Weather_Converter/weather.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 // Function to convert Celsius to Fahrenheit
@@ -10,6 +11,21 @@ double convertF(double temp ){
 double convertC(double temp){
         return (temp - 32) * 5/9;
 };
+
+// Function message output
+void weatherMessage(double temperature){
+    if (temperature > 100) {
+        cout << "Extremely hot!\n";
+    } else if (temperature > 80) {
+        cout << "It's too hot outside!\n";
+    } else if (temperature > 75) {
+        cout << "It's a bit warm!\n";
+    } else if (temperature > 50) {
+        cout << "It's a bit chilly!\n";
+    } else {
+        cout << "It's cold!\n";
+    }
+};
         
 
 
@@ -17,8 +33,7 @@ int main(){
     
     int choice;
      // store user's yes/no input
-    
-   
+
     do {
         double num; // stores user's Celsius input
         cout << "===== WEATHER CONVERTER ===== \n";
@@ -33,22 +48,25 @@ int main(){
         if(choice == 1) {
             cout << "Enter number: ";
             cin >> num;
-            double fahrenheit = convertF(num); 
+            double fahrenheit = convertF(num); // convert call function
             cout << "The temperature is: " << fahrenheit << " F\n";
+            weatherMessage(fahrenheit); // call function
 
         } else if (choice == 2) {
             cout << "Enter number: ";
             cin >> num;
             double celsius = convertC(num);
             cout << "The temperature is: " << celsius << " C\n";
+            weatherMessage(celsius);
+
         } else if (choice == 3) {
             cout << "Goodbye!\n";
-
 
         }else {
             cout << "Invalid input. Please choose 1, 2, 3.\n";
         }
     } while (choice != 3);
+    return 0;
 
    
 
