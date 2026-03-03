6d679aa766d2bbcd4ff94ecceafbbb9e8602b9d3
denverinthrough-crypto <denverinthrough@gmail.com>
2026-03-03 14:52:42 -0500

    commented all rider's profile, then practicing building teams

--- Cycling_Teams/tourTeams.cpp
+++ Cycling_Teams/tourTeams.cpp
@@ -4,87 +4,143 @@
 using namespace std;
 
 
-struct Rider {
-    string fname, lname, country, role;
+class Teams {
+    private:
+ 
+        vector<string> teams;
+
+    public: 
+        void addTeam(string name){
+            teams.push_back(name);
+            cout << "Team " + name + " added.\n";
+        }
+
+        void showTeam(){
+            cout << "\n****| Tour de France Teams |****\n";
+            for(int i = 0; i < teams.size(); i++) {
+                cout << i + 1 << ". " << teams[i] << "\n";
+            }
+        }
+
 };
 
+int main() {
 
-int main(){
-    
-    
-    
-    vector<Rider> roster;
-    int roleOption;
-    string choice;
-    
-    
-    cout << "********** RIDER's PROFILE **********\n\n";
+    Teams tourTeams;
+    string teamName;
+    int choice;
 
-    
-        Rider rider;
-    
+    do {
+        cout << "\n*** Tour De France Teams ***\n";
+        cout << "1. Add Team\n2. Show Teams\n3. Exit\n";
+        cout << "Choose: ";
+        cin >> choice;
+        cin.ignore();
 
-
-    do {   
-        // ask user to enter name
-        cout << "Enter First Name: ";
-        getline(cin, rider.fname);
-
-        cout << "Enter Last Name: ";
-        getline(cin,  rider.lname);
-
-        cout << "Enter Country: ";
-        getline(cin, rider.country);
-
-        string profile = rider.fname + " " + rider.lname + " : " + rider.country;
-        cout << "Rider's profile: " + profile << "\n";
-
-        cout << "\nAssign Role:\n";
-        cout << "1. Team Leader\n";
-        cout << "2. Climbing Domestique\n";
-        cout << "3. All-Around Domestique\n";
-        cin >> roleOption;
-
-    
-   
-        switch(roleOption) {
+        switch(choice){
             case 1:
-                rider.role = "Team Leader";
+                cout << "Enter team name: ";
+                getline(cin, teamName);
+                tourTeams.addTeam(teamName);
                 break;
             case 2:
-                rider.role = "Climbing Domestique";
+                tourTeams.showTeam();
                 break;
             case 3:
-                rider.role = "All-Around Domestique";
+                cout << "Exiting.... \n";
                 break;
             default:
                 cout << "Invalid option.\n";
-                continue;
+
         }
+    } while (choice != 3);
 
-        roster.push_back(rider); // save rider to roster
-        cout <<  rider.role  + " : " + profile + "\n"; // press enter to add another rider
-        cout << "****|Team Roster|****\n";
+    return 0;
+   
 
-        for(int i = 0; i < roster.size(); i++){
+}
+
+// struct Rider {
+//     string fname, lname, country, role;
+// };
+
+
+// int main(){
+    
+    
+    
+//     vector<Rider> roster;
+//     int roleOption;
+//     string choice;
+    
+    
+//     cout << "********** RIDER's PROFILE **********\n\n";
+
+    
+//         Rider rider;
+    
+
+
+//     do {   
+//         // ask user to enter name
+//         cout << "Enter First Name: ";
+//         getline(cin, rider.fname);
+
+//         cout << "Enter Last Name: ";
+//         getline(cin,  rider.lname);
+
+//         cout << "Enter Country: ";
+//         getline(cin, rider.country);
+
+//         string profile = rider.fname + " " + rider.lname + " : " + rider.country;
+//         cout << "Rider's profile: " + profile << "\n";
+
+//         cout << "\nAssign Role:\n";
+//         cout << "1. Team Leader\n";
+//         cout << "2. Climbing Domestique\n";
+//         cout << "3. All-Around Domestique\n";
+//         cin >> roleOption;
+
+    
+   
+//         switch(roleOption) {
+//             case 1:
+//                 rider.role = "Team Leader";
+//                 break;
+//             case 2:
+//                 rider.role = "Climbing Domestique";
+//                 break;
+//             case 3:
+//                 rider.role = "All-Around Domestique";
+//                 break;
+//             default:
+//                 cout << "Invalid option.\n";
+//                 continue;
+//         }
+
+//         roster.push_back(rider); // save rider to roster
+//         cout <<  rider.role  + " : " + profile + "\n"; // press enter to add another rider
+//         cout << "****|Team Roster|****\n";
+
+//         for(int i = 0; i < roster.size(); i++){
             
-            cout << i + 1 << ". "
-                << roster[i].fname + " " +  roster[i].lname 
-                << " | " << roster[i].country
-                << " | " << roster[i].role << "\n";
-        }
+//             cout << i + 1 << ". "
+//                 << roster[i].fname + " " +  roster[i].lname 
+//                 << " | " << roster[i].country
+//                 << " | " << roster[i].role << "\n";
+//         }
 
-        cout << "Add another rider: (yes/no)\n"; // i need a loop to go back to this option
-        cin >> choice;
-        cin.ignore();
+//         cout << "Add another rider: (yes/no)\n"; // i need a loop to go back to this option
+//         cin >> choice;
+//         cin.ignore();
         
 
-    } while (choice == "Yes" || choice == "yes");
-        cout << "Finished team's roster.\n";
+//     } while (choice == "Yes" || choice == "yes");
+//         cout << "Finished team's roster.\n";
    
-    return 0;
+//     return 0;
 
-};
+// };
 
 
 
